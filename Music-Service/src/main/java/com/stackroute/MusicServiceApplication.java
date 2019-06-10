2202fb7e44e810edb4df1d8beaabd03e76a74670
rohit <ganguru.rohith@gmail.comclea>
2019-06-10 15:26:11 +0530

    MUsic-Service eureka done

--- Music-Service/src/main/java/com/stackroute/MusicServiceApplication.java
+++ Music-Service/src/main/java/com/stackroute/MusicServiceApplication.java
@@ -2,8 +2,10 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //Main Class
+@EnableEurekaClient
 @SpringBootApplication
 public class MusicServiceApplication {
 
