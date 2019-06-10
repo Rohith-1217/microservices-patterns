2ecdf4585c643ca36563f529cba6b4ad8e066fa6
rohit <ganguru.rohith@gmail.comclea>
2019-06-10 10:57:13 +0530

    user and music micro services

---
+++ userservice/src/main/java/com/stackroute/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.stackroute.controller;
+
+import com.stackroute.domain.User;
+import com.stackroute.exception.UserAlreadyExistsException;
+import com.stackroute.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1")
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService)
+    {
+        this.userService=userService;
+    }
+
+    @PostMapping("user")
+    public ResponseEntity<?> saveUser(@RequestBody User user)
+    {
+        ResponseEntity responseEntity;
+        try{
+            userService.saveUser(user);
+            responseEntity=new ResponseEntity("successfully created", HttpStatus.CREATED);
+        }catch (UserAlreadyExistsException ex)
+        {
+            responseEntity=new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
+            ex.printStackTrace();
+        }
+        return responseEntity;
+    }
+
+    @GetMapping("user")
+    public ResponseEntity<?> getAllUsers()
+    {
+        return new ResponseEntity<List<User>>(userService.getAllUsers(), HttpStatus.OK);
+    }
+}
